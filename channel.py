88a7c5b24cccc1451eb951855fe2b6cc43904ef7
Michał Bernacki-Janson <94766391+Trylog@users.noreply.github.com>
2023-03-27 20:20:29 +0200

    first

---
+++ channel.py
@@ -0,0 +1,14 @@
+import komm
+
+def bsc(probability, packet):
+    bsc = komm.BinarySymmetricChannel(probability)
+    return bsc(packet)
+
+def gem(probability, packet):
+
+    return
+def chIn():
+
+
+def chOut():
+
