e8b90c6f65a6383aa81f3836a091e2fa0d0fe68f
Michał Bernacki-Janson <94766391+Trylog@users.noreply.github.com>
2023-05-05 14:38:27 +0200

    Changes by Justy

--- main.py
+++ main.py
@@ -5,12 +5,45 @@ import numpy as np
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+menu_options1 = {
+    1: 'ARQ step and wait',
+    2: 'ARQ selective repeat',
+    3: 'Wyjście',
+}
 
+menu_options2 = {
+    1: 'BST',
+    2: 'Geblerta-Eliota',
+}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def menu():
+    while (True):
+        print('Wybierz typ ARQ')
+        print_menu(1)
+        option1 = int(input('Wybrano: '))
+        print('Wybierz typ kanału')
+        print_menu(2)
+        option2 = int(input('Wybrano: '))
+        if option1 == 3:
+            return 0,0;
+        elif option1 == 1 or option1 == 2:
+            if option2 == 1 or option2 == 2:
+                return option1, option2
+        else:
+            print('Podano złe wartości')
 
+def print_menu(i):
+    if i == 1:
+        for key in menu_options1.keys():
+            print(key, ':', menu_options1[key])
+    if i == 2:
+        for key in menu_options2.keys():
+            print(key, ':', menu_options2[key])
+
+if __name__ == '__main__':
+    #data = bytes([1, 2, 3, 4])
+    #print(sys.getsizeof(data))
+    options = menu();
 
 # Press the green button in the gutter to run the script.
 
